bbda509e33a9c69537a0e6cf939d42953ef8d7fc
mukulBillore <mukulbillore88@gmail.com>
2022-03-08 10:31:13 +0530

    Updated : added the user input name by two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello angular by Mukul Billore ';
+  userName:string ='';
   imageUrl= "E:\Angular\AngularStart\src\assets"
   url="https://bridgelabz.com/";
   onClick($event: any){
     console.log("Image is clicked!",$event);
     window.open(this.url,"_blank");
   }
+
+  onInput($event:any){
+    console.log("Change event occured!",$event.data);
+ //   const nameRegex=RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+   // if(nameRegex.test(this.userName)){
+   //   this.nameError="";
+      return;
+    //}
+   // this.nameError="Name is incorrect"
+//  }
+
+}
 }
