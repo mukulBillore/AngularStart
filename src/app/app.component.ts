b917d4ff85c643ae979357ad176c7b2a7ce4fd49
mukulBillore <mukulbillore88@gmail.com>
2022-03-08 10:14:43 +0530

    Updated : added the image through one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularStart';
+  title = 'hello angular by Mukul Billore ';
+  imageUrl= "E:\Angular\AngularStart\src\assets"
+  ngOnInIt():void {
+    this.title = "hello mukul";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
+//app.module.ts :- Indicates that it is the type script file provides strong 
+//                 type checking.
+
+
+// core moduldes of angular 
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+// when ever CLI is used to genrate the new components and services,it will 
+// it will automatically update this file to import and add them here.   
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// further using @NgModule decorator we provide additional metadata to specify 
+// components ,the services ,the imports etc further decorators are used while 
+// processing,instantiation and at runtime 
 @NgModule({
+
+  // components are added here ,Essentially the classes that has views ,they are components 
+  // directives and pipes.
   declarations: [
     AppComponent
   ],
+
+  // various imports are needed for Applocation can be added here 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+
+  // Typically serves like http-services are added to providers 
   providers: [],
+
+  //  the root component which is the main view of the application.Only the 
+  //  root module  has the property it indecates the component that's going to be bootstrapped
   bootstrap: [AppComponent]
 })
 export class AppModule { }
